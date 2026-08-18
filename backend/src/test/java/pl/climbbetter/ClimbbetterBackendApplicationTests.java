fe7ec39b8d3d07fa371833386e74cf35f56fcdc7
mihuwis <mihuwis@gmail.com>
2026-08-18 19:30:41 +0200

    Add clock tests

--- backend/src/test/java/pl/climbbetter/ClimbbetterBackendApplicationTests.java
+++ backend/src/test/java/pl/climbbetter/ClimbbetterBackendApplicationTests.java
@@ -1,17 +1,30 @@
 package pl.climbbetter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import java.time.Clock;
+import java.time.ZoneOffset;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 @ActiveProfiles("test")
 class ClimbbetterBackendApplicationTests {
 
+	@Autowired
+	Clock clock;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void clockUsesUtcZone(){
+		assertEquals(ZoneOffset.UTC, clock.getZone());
 	}
 
 }
